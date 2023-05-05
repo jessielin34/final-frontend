707761e88e06d3d515abce967cbd287790d595ed
jessielin34 <80348079+jessielin34@users.noreply.github.com>
2023-05-05 16:51:53 -0400

    updated the single course view

--- src/components/views/CourseView.js
+++ src/components/views/CourseView.js
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 const CourseView = (props) => {
   const { course } = props;
   return (
-    <div>
+    <div style={{backgroundColor:"#FFC0CB",color:"black",fontFamily:"Roboto",display:"block",padding:20}}>
       <h1>{course.title}</h1>
-      {course.instructor ? <h3>{course.instructor.firstname + " " + course.instructor.lastname}</h3>: <h3>staff</h3>}
+      {course.instructor ? <h3>{course.instructor.firstname + " " + course.instructor.lastname}</h3>: <h3>staff (unassigned)</h3>}
       <Link to={`/editcourse/${course.id}`}>Edit course information</Link>
       <br/>
       <Link to={`/courses`}>View all courses</Link>
+      <div style={{backgroundColor:"#FFC0CB",color:"black",fontFamily:"Roboto",display:"block",padding:20}}></div>
     </div>
   );
 
